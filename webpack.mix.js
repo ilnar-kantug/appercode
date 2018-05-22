4bc490edbbf16e99868f7a0d322582d23934edce
ilnar_kant <ilnar.kantug@yandex.ru>
2018-05-22 22:58:22 +0300

    Changed laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('/build/')
+    .js('resources/assets/js/app.js', 'js')
+    .sass('resources/assets/sass/app.scss', 'css')
+    .version();
